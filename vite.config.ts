843f2f2b85c7ba82b096f0fced6b4654d2b2fed6
jeffhu1 <jeffhu@stanford.edu>
2022-10-05 16:23:00 -0500

    fix deployment

--- vite.config.ts
+++ vite.config.ts
@@ -27,7 +27,6 @@ export default defineConfig({
   },
   build: {
     target: "es2020",
-    outDir: "build",
     sourcemap: true,
     rollupOptions: {
       plugins: [nodePolyfills({ include: null })],
